eb2f8db193ac4cb32f463f35762aeb72d2d36ad5
Tony Ashworth <digitalcassette@gmail.com>
2023-12-06 01:13:23 -0500

    completely forgot about ranges that extend beyond both boundaries

--- day-5-seedmaps-part-two.ts
+++ day-5-seedmaps-part-two.ts
@@ -38,7 +38,7 @@ function processAdjustmentData(line: string) {
 function calculate() {
     for (var mapIndex = 0; mapIndex < adjustmentMap.length; mapIndex++) {
         const map = adjustmentMap[mapIndex];
-        console.log(`Processing adjustment map ${JSON.stringify(map)}`);
+        // console.log(`Processing adjustment map ${JSON.stringify(map)}`);
         for (var seedIndex = 0; seedIndex < seedInputs.length; seedIndex++) {
             const seedCopy = seedInputs[seedIndex];
             if (seedCopy.processed) {
@@ -47,47 +47,61 @@ function calculate() {
             if (seedCopy.start >= map.fromLower && seedCopy.start <= map.fromHigher) {
                 if (seedCopy.end <= map.fromHigher) {
                     // range fully encompased so just add deltas
-                    console.log(`Range fully encompassed`);
+                    // console.log(`Range fully encompassed`);
                     seedInputs[seedIndex].start += map.delta;
                     seedInputs[seedIndex].end += map.delta;
                     seedInputs[seedIndex].processed = true;
-                    console.log(`Processed range ${JSON.stringify(seedCopy)} into ${JSON.stringify(seedInputs[seedIndex])} with delta ${map.delta}`);
+                    // console.log(`Processed range ${JSON.stringify(seedCopy)} into ${JSON.stringify(seedInputs[seedIndex])} with delta ${map.delta}`);
                 } else {
-                    console.log(`Partial match on start of range`);
+                    // console.log(`Partial match on start of range`);
 
                     // range extends beyond the map (start is in, end is out)
                     seedInputs[seedIndex].start += map.delta;
                     seedInputs[seedIndex].end = map.fromHigher + map.delta;
                     seedInputs[seedIndex].processed = true;
-                    console.log(`Processed range ${JSON.stringify(seedCopy)} into ${JSON.stringify(seedInputs[seedIndex])} with delta ${map.delta}`);
+                    // console.log(`Processed range ${JSON.stringify(seedCopy)} into ${JSON.stringify(seedInputs[seedIndex])} with delta ${map.delta}`);
 
                     // create new input of the unmatched portion
                     let newSeedInput = {start: map.fromHigher + 1, end: seedCopy.end, processed: false};
                     seedInputs.push(newSeedInput);
-                    console.log("Created New Input " + JSON.stringify(newSeedInput));
+                    // console.log("Created New Input " + JSON.stringify(newSeedInput));
                 }
             } else if (seedCopy.end >= map.fromLower && seedCopy.end <= map.fromHigher) {
-                console.log(`Partial match on end of range`);
+                // console.log(`Partial match on end of range`);
 
                 // range extends beyond the map (end is in, start is out)
                 seedInputs[seedIndex].start = map.fromLower + map.delta;
                 seedInputs[seedIndex].end += map.delta;
                 seedInputs[seedIndex].processed = true;
-                console.log(`Processed range ${JSON.stringify(seedCopy)} into ${JSON.stringify(seedInputs[seedIndex])} with delta ${map.delta}`);
+                // console.log(`Processed range ${JSON.stringify(seedCopy)} into ${JSON.stringify(seedInputs[seedIndex])} with delta ${map.delta}`);
 
                 // create new input of the unmatched portion
                 let newSeedInput = {start: seedCopy.start, end: map.fromLower - 1, processed: false};
                 seedInputs.push(newSeedInput);
-                console.log("Created New Input " + JSON.stringify(newSeedInput));
+                // console.log("Created New Input " + JSON.stringify(newSeedInput));
+            } else if (seedCopy.start < map.fromLower && seedCopy.end > map.fromHigher) {
+                // console.log(`Range extends outside of the map in both directions :facepalm:`);
+
+                // range extends beyond the map in both directions
+                seedInputs[seedIndex].start = map.fromLower + map.delta;
+                seedInputs[seedIndex].end = map.fromHigher + map.delta;
+                seedInputs[seedIndex].processed = true;
+
+                // create new input of the unmatched portions
+                let newSeedInputLower = {start: seedCopy.start, end: map.fromLower - 1, processed: false};
+                seedInputs.push(newSeedInputLower);
+
+                let newSeedInputUpper = {start: map.fromHigher + 1, end: seedCopy.end, processed: false};
+                seedInputs.push(newSeedInputUpper);
             } else {
                 // console.log(`skip`);
                 // range is outside of the map so do nothing
             }
         }
     }
-    for (var seedIndex = 0; seedIndex < seedInputs.length; seedIndex++) {
-        seedInputs[seedIndex].processed = false;
-    }
+    seedInputs.forEach(element => {
+        element.processed = false;
+    });
 }
 
 lineReader.on('line', (line) => {
@@ -95,10 +109,10 @@ lineReader.on('line', (line) => {
         parseDesiredSeeds(line);
         console.log(`Seed inputs are ${JSON.stringify(seedInputs)}`);
     } else if (line.includes("-to-")) {
-        console.log(`--- --- processing section for ${line}`);
+        // console.log(`--- --- processing section for ${line}`);
         // do nothing
     } else if (line.trim() === "") {
-        console.log(`Adjustment map is ${JSON.stringify(adjustmentMap)}`);
+        // console.log(`Adjustment map is ${JSON.stringify(adjustmentMap)}`);
         // calculate adjustments
         calculate();
         // clear adjustment map
@@ -111,11 +125,14 @@ lineReader.on('line', (line) => {
 lineReader.on('close', () => {
     var closestLocation = Number.MAX_SAFE_INTEGER;
 
+    var count = 0;
+
     seedInputs.forEach(element => {
+        count++;
         closestLocation = Math.min(closestLocation, element.start);
     });
 
-    console.log(`Closest location is ${closestLocation}`);
+    console.log(`Closest location is ${closestLocation} with ${count} seeds`);
 });
 
 // final answer is 54632
